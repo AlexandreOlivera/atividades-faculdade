9c77185d0cd45d3cbeb9f50ac3f5dfddd1492001
Alexandre Oliveira <o.alexandrej14@gmail.com>
2024-11-26 17:43:58 -0300

    configurando a aplicação principal

--- projeto2-frameworks-web/frontend-atividade2-frameworks-web/frontend/src/App.js
+++ projeto2-frameworks-web/frontend-atividade2-frameworks-web/frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { getFilms, createFilm, updateFilm, deleteFilm } from './services/filmService';
+import FilmForm from './components/FilmForm';
+import FilmList from './components/FilmList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [films, setFilms] = useState([]);
+    const [editingFilm, setEditingFilm] = useState(null);
+
+    useEffect(() => {
+        getFilms().then(res => setFilms(res.data));
+    }, []);
+
+    const handleCreate = (film) => {
+        createFilm(film).then(res => setFilms([...films, res.data]));
+    };
+
+    const handleUpdate = (film) => {
+        updateFilm(editingFilm._id, film).then(res => {
+            setFilms(films.map(f => (f._id === res.data._id ? res.data : f)));
+            setEditingFilm(null);
+        });
+    };
+
+    const handleDelete = (id) => {
+        deleteFilm(id).then(() => setFilms(films.filter(f => f._id !== id)));
+    };
+
+    return (
+        <div>
+            <h1>Film Management</h1>
+            <FilmForm onSubmit={editingFilm ? handleUpdate : handleCreate} initialData={editingFilm} />
+            <FilmList films={films} onEdit={setEditingFilm} onDelete={handleDelete} />
+        </div>
+    );
+};
 
 export default App;
