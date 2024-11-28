c7e2d500538f668aa7660de598d4c95046bd20af
Alexandre Oliveira <o.alexandrej14@gmail.com>
2024-11-28 13:58:50 -0300

    alterações de melhora no código

--- projeto2-dispo-moveis/gerenciador_de_filmes/lib/main.dart
+++ projeto2-dispo-moveis/gerenciador_de_filmes/lib/main.dart
@@ -3,14 +3,20 @@ import 'screens/lista_filmes.dart';
 import 'screens/cadastro_filme.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Adiciona suporte para chave no widget.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Gerenciador de Filmes',
+      debugShowCheckedModeBanner: false, // Remove o banner "DEBUG".
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Define um tema padrão para o app.
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => ListaFilmes(),
